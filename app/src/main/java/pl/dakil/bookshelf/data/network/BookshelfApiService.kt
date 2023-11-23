375c58251aeef50a57d21fb986ae78746da6b4d9
palinkiewicz <pogromca.ap@gmail.com>
2023-11-23 20:16:00 +0100

    Change return types in API service to get Response data

--- app/src/main/java/pl/dakil/bookshelf/data/network/BookshelfApiService.kt
+++ app/src/main/java/pl/dakil/bookshelf/data/network/BookshelfApiService.kt
@@ -2,15 +2,16 @@ package pl.dakil.bookshelf.data.network
 
 import pl.dakil.bookshelf.data.model.Book
 import pl.dakil.bookshelf.data.model.BooksQueryResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface BookshelfApiService {
     @GET("volumes")
-    suspend fun getBooks(@Query("q") q: String): List<BooksQueryResponse>?
+    suspend fun getBooks(@Query("q") q: String): Response<BooksQueryResponse>?
 
     @GET("volumes/{id}")
-    suspend fun getBook(@Path("id") id: String): Book?
+    suspend fun getBook(@Path("id") id: String): Response<Book>?
 
 }
