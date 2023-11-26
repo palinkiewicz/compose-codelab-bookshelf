6d56ed2549562a8ce5b1b1790a558a250602717d
palinkiewicz <pogromca.ap@gmail.com>
2023-11-26 15:36:39 +0100

    Refactor Book data class

--- app/src/main/java/pl/dakil/bookshelf/data/model/Book.kt
+++ app/src/main/java/pl/dakil/bookshelf/data/model/Book.kt
@@ -3,19 +3,23 @@ package pl.dakil.bookshelf.data.model
 data class Book (
     val id: String,
     val volumeInfo: VolumeInfo
-)
-
-data class VolumeInfo (
-    val title: String,
-    val authors: List<String>?,
-    val publisher: String,
-    val publishedDate: String,
-    val description: String,
-    val language: String,
-    val imageLinks: ImageLinks?
-)
-
-data class ImageLinks (
-    val thumbnail: String,
-    val smallThumbnail: String
-)
+) {
+    companion object {
+        data class VolumeInfo (
+            val title: String,
+            val authors: List<String>?,
+            val publisher: String,
+            val publishedDate: String,
+            val description: String,
+            val language: String,
+            val imageLinks: ImageLinks?
+        ) {
+            companion object {
+                data class ImageLinks (
+                    val thumbnail: String,
+                    val smallThumbnail: String
+                )
+            }
+        }
+    }
+}
