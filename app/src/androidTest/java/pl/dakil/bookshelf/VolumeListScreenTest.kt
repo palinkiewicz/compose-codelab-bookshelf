4507855570d31f1d4f2156f2dbde38541754df87
palinkiewicz <pogromca.ap@gmail.com>
2024-01-13 19:09:02 +0100

    Create clicked on search bar test and add comments

--- app/src/androidTest/java/pl/dakil/bookshelf/VolumeListScreenTest.kt
+++ app/src/androidTest/java/pl/dakil/bookshelf/VolumeListScreenTest.kt
@@ -3,7 +3,7 @@ package pl.dakil.bookshelf
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -35,11 +35,41 @@ class VolumeListScreenTest {
 
     @Test
     fun volumeListScreen_initialized_searchBarVisible() {
-        composeTestRule.onRoot(useUnmergedTree = true)
-
+        // Assert the search bar is visible
         composeTestRule.onNodeWithStringId(R.string.search_volumes).assertIsDisplayed()
+
+        // Assert the search icon is visible
         composeTestRule
             .onNodeWithTagForStringId(R.string.search_icon, useUnmergedTree = true)
             .assertIsDisplayed()
     }
+
+    @Test
+    fun volumeListScreen_clickedOnSearchBar_iconChangedToBackArrow() {
+        // Click on the search bar
+        composeTestRule.onNodeWithStringId(R.string.search_volumes).performClick()
+
+        // Assert the back icon is visible
+        composeTestRule
+            .onNodeWithTagForStringId(R.string.back_icon, useUnmergedTree = true)
+            .assertIsDisplayed()
+    }
+
+    // This will not work unless other submitting method is created
+    // TODO: Create a search button to make VolumeListScreen tests possible
+//    @Test
+//    fun volumeListScreen_searched_multipleVolumesDisplayed() = runTest {
+//        val input = "ABC"
+//
+//        // Input text into the search bar and search
+//        composeTestRule.onNodeWithStringId(R.string.search_volumes).apply {
+//            performTextInput(input)
+//            performImeAction()
+//        }
+//
+//        // Assert each volume is visible
+//        repository.getBooks(input)!!.forEach {
+//            composeTestRule.onNodeWithText(it.volumeInfo.title).assertIsDisplayed()
+//        }
+//    }
 }
